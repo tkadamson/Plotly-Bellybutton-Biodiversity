3c30edce9c5fda0678ffc40fe96205948f1642cc
Thomas Adamson <thomasadamson@Thomass-MacBook-Air-2.local>
2021-04-03 18:53:03 -0700

    Completed bar chart portion of the project

--- static/js/app.js
+++ static/js/app.js
@@ -26,4 +26,32 @@ let personalData = demographics.filter(function (person) {
  
   console.log(sampleData);
 
-});
+  //Get individual components out of the sampleData object
+  let otu_ids = sampleData[0].otu_ids.slice(0,10).reverse();
+  console.log(otu_ids);
+
+  let sample_values = sampleData[0].sample_values.slice(0,10).reverse();
+  console.log(sample_values);
+
+  let otu_labels = sampleData[0].otu_labels.slice(0, 10);
+  console.log(otu_labels);
+
+  //Use data in a trace
+  let trace1 = {
+      x: sample_values,
+      y: otu_ids,
+      text: otu_labels,
+      type: "bar",
+      orientation: "h"
+  };
+
+  let layout = {
+    title: "Most Commonly Found OTU's",
+    xaxis: {title: "OTU Count"},
+    yaxis: {type: "category", title: "OTU ID Number"}
+  }
+
+  let barData = [trace1];
+
+  Plotly.newPlot("bar", barData, layout);
+}); 
