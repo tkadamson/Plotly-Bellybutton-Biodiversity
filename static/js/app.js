ba2810c1ffd45ace5b789db2ea68f092da07f5fc
Thomas Adamson <thomasadamson@Thomass-MacBook-Air-2.local>
2021-04-03 18:20:00 -0700

    Read in JSON data and filtered down to the specified id. Temporarily static id

--- static/js/app.js
+++ static/js/app.js
@@ -1,3 +1,29 @@
-d3.json('samples.json').then(data => {
-    console.log(data);
-})
+d3.json('samples.json').then(function(data) {
+
+console.log(data);
+
+//Read demographic and sample data
+let demographics = data.metadata;
+console.log(demographics);
+
+let samples = data.samples;
+console.log(samples);
+
+//Set an id (temporarily static, will use event listener later)
+let id = 940;
+let strID = String(id);
+
+//Filter the json data to match the id
+let personalData = demographics.filter(function (person) {
+   return person.id === id;
+});
+
+ console.log(personalData);
+
+ let sampleData = samples.filter(function (person) {
+    return person.id === strID;
+ });
+ 
+  console.log(sampleData);
+
+});
