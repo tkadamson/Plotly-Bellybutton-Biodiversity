7b6b0accfe2321a4c95cab920191a0c70c279d7c
Thomas Adamson <thomasadamson@Thomass-MacBook-Air-2.local>
2021-04-06 11:04:07 -0700

    Added gauge chart and needle. Needle is not currently live to counter, set static vertical until I can correctly position it at all numbers

--- static/js/app.js
+++ static/js/app.js
@@ -14,8 +14,6 @@ genPlots('940');
 
 function genPlots(id) {
 
-   console.log(id);
-
    //Filter the json data to match the id
    let personalData = demographics.filter(function (person) {
       return person.id === Number(id);
@@ -30,6 +28,7 @@ function genPlots(id) {
    console.log(sampleData);
 
    //Get individual components out of the sampleData object
+   //Both full dataset (for bubble) and top 10 (for bar)
    let otu_ids = sampleData[0].otu_ids;
    let otu_ids10 = otu_ids.slice(0,10).reverse();
    console.log(otu_ids10);
@@ -51,7 +50,7 @@ function genPlots(id) {
          orientation: "h"
    };
 
-   let layout = {
+   let barLayout = {
       title: "Most Commonly Found OTU's",
       xaxis: {title: "OTU Count"},
       yaxis: {type: "category", title: "OTU ID Number"}
@@ -59,7 +58,7 @@ function genPlots(id) {
 
    let barData = [trace1];
 
-   Plotly.newPlot("bar", barData, layout);
+   Plotly.newPlot("bar", barData, barLayout);
 
    //Place demographic info in a table
    //Get keys and values separately
@@ -79,18 +78,58 @@ function genPlots(id) {
    for (let i = 0; i < demo_labels.length; i++) {
 
       let row = tbody.append("tr");
-
+      
+      //Append the left cell (label) first, with bold
       let cell1 = row.append("td");
       cell1.text(demo_labels[i]); 
       cell1.style("font-weight", "bold");
       cell1.style("padding-left", "10px");
       cell1.style("padding-right", "30px");
 
-
+      //Append data to the right
       let cell2 = row.append("td");
       cell2.text(demo_info[i]);
    };
 
+   //Gauge Chart
+   //Copied from https://code.tutsplus.com/tutorials/create-interactive-charts-using-plotlyjs-pie-and-gauge-charts--cms-29216
+   //with modifications to needle positioning and color scheme
+
+   let trace3 = {
+      type: 'pie',
+      hole: 0.4,
+      rotation: 90,
+      showlegend: false,
+      values: [ 81/9, 81/9, 81/9, 81/9, 81/9, 81/9, 81/9, 81/9, 81/9, 81],
+      text: ['0','1','2','3','4','5','6','7','8+'],
+      direction: 'clockwise',
+      textinfo: 'text',
+      textposition: 'inside',
+      marker: {
+         colors: ['rgba(61, 193, 67, 0.2)', 'rgba(61, 193, 67, 0.3)', 'rgba(61, 193, 67, 0.4)', 'rgba(61, 193, 67, 0.5)', 'rgba(61, 193, 67, 0.6)', 'rgba(61, 193, 67, 0.7)', 'rgba(61, 193, 67, 0.8)', 'rgba(61, 193, 67, 0.9)', 'rgb(61, 193, 67)', 'white'],
+        labels: ['0-1','1-2','2-3','3-4','4-5','5-6','6-7','7-8','8-9'],
+      }
+   };
+ 
+let gaugeLayout = {
+   width: 750, 
+   height: 500,
+   shapes:[{
+      type: 'line',
+      x0: 0.5,
+      y0: 0.5,
+      x1: 0.5,
+      y1: 0.8,
+      line: {
+        color: 'black',
+        width: 3
+      }
+    }]
+   };
+
+   let gaugeData = [trace3];
+
+   Plotly.newPlot('gauge', gaugeData, gaugeLayout); 
    //Bubble Chart
 
    let trace2 = {
